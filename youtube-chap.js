f57bbf1f3cc08a9c3c0304c699bc06977664dcab
SenorGato <senoraraton@gmail.com>
2022-01-18 07:50:23 +0000

    It works, sortof

--- youtube-chap.js
+++ youtube-chap.js
@@ -1,11 +1,8 @@
-console.log('Hack the Planet');
-document.body.style.border = "20px solid red";
-
 const target = document.getElementById('content');
 const observer = new MutationObserver(muts => {
     console.log(muts);
 });
-const config = { charachterData: false, attributes: false, childList:false, subtree: attributes}
+const config = { charachterData: true, attributes: true, childList: true, subtree: true}
 observer.observe(target, config);
 
 //    var x = document.getElementById("content").innerText;
