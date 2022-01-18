233c2863a18eedc2499e5b7e731c0da0945b5af0
SenorGato <senoraraton@gmail.com>
2022-01-18 07:33:34 +0000

    Mutation changes

--- youtube-chap.js
+++ youtube-chap.js
@@ -1,15 +1,12 @@
 console.log('Hack the Planet');
 document.body.style.border = "20px solid red";
 
-const targetNode = document.getElementById('content');
+const target = document.getElementById('content');
 const observer = new MutationObserver(muts => {
     console.log(muts);
-    console.log("Motherfucker");
 });
-
-observer.observe(targetNode, {
-        attributes: true
-});
+const config = { charachterData: false, attributes: false, childList:false, subtree: attributes}
+observer.observe(target, config);
 
 //    var x = document.getElementById("content").innerText;
 //    console.log(x);
