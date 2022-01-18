0530c0dd9e17a4fa3b37838f5a771f9d408b430f
SenorGato <senoraraton@gmail.com>
2022-01-18 07:17:00 +0000

    Mutation observer failures

--- youtube-chap.js
+++ youtube-chap.js
@@ -1,2 +1,15 @@
 console.log('Hack the Planet');
 document.body.style.border = "20px solid red";
+
+const targetNode = document.getElementById('content');
+const observer = new MutationObserver(muts => {
+    console.log(muts);
+    console.log("Motherfucker");
+});
+
+observer.observe(targetNode, {
+        attributes: true
+});
+
+//    var x = document.getElementById("content").innerText;
+//    console.log(x);
